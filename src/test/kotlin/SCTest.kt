184dabf80c27dd6872fdd2af369ab3363b9de816
Efra Espada <efraespada@gmail.com>
2019-06-06 18:10:30 +0200

    ...

--- src/test/kotlin/SCTest.kt
+++ src/test/kotlin/SCTest.kt
@@ -5,8 +5,6 @@ class SCTest {
 
     private val logger by logger()
 
-    private val gradle = "/usr/local/Cellar/gradle/4.10.2/libexec/bin/gradle"
-
     @Test
     fun `terminal`() {
         val expectedMessage = "string_test"
@@ -16,7 +14,7 @@ class SCTest {
 
 
     @Test
-    fun `gradle build`() {
+    fun `gradlew signingReport`() {
         val signingReport = Executor.execute(
             """
             rm -rf KotlinSample &&
@@ -28,50 +26,6 @@ class SCTest {
         """.trimIndent()
         )
         assert(signingReport.contains("SHA1") && signingReport.contains("BUILD SUCCESSFUL"))
-    }
-
-    @Test
-    fun `1 gradle build`() {
-        //val build = Executor.execute("./gradlew")
-        //logger.debug(build)
-    }
-
-    @Test
-    fun `get fingerprint`() {
-        /*
-        Executor.execute("rm -rf KotlinSample")
-        Executor.execute("git clone https://github.com/StringCare/KotlinSample.git")
-        val moveAndShow = Executor.execute("cd KotlinSample && ./gradlew")
-        logger.debug(moveAndShow)
-        val resC = Executor.execute("./gradlew clean")
-        logger.debug(resC)
-         */
-    }
-
-    @Test
-    fun firstMethodTest() {
-        // verify(shellHandler.shell)?.exec("rm -rf KotlinSample")
-
-
-        assert(true)
-/*
-
-
-        val processA = commandTest("rm -rf KotlinSample")
-        processA.stringOutput {
-            logger.debug(it)
-        }
-        val exitCodeA = processA.waitFor()
-        assert(exitCodeA == 0)
-        val processB = commandTest("git clone https://github.com/StringCare/KotlinSample.git")
-        val exitCodeB = processB.waitFor()
-        assert(exitCodeB == 0)
-         */
-    }
-
-    @Test
-    fun secondMethodTest() {
-        assert(true)
     }
 
 }
